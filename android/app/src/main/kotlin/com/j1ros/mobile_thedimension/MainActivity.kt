44f6ade2562d9d588a7422078c2954d202fe17cc
j1ros <j1ros@mail.ru>
2020-01-16 14:52:53 +0300

    first commitinit

---
+++ android/app/src/main/kotlin/com/j1ros/mobile_thedimension/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.j1ros.mobile_thedimension
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
